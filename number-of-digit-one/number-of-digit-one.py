267cd76b25a604dd55be6ed49f1b7c444d4d046e
Qasim Wani <43754306+QasimWani@users.noreply.github.com>
2020-12-20 13:41:20 +0300

    Time: 24 ms (91.89%), Space: 14.4 MB (8.11%) - LeetHub

---
+++ number-of-digit-one/number-of-digit-one.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countDigitOne(self, n):
+        if n <= 0:
+            return 0
+        if 1 <= n <= 9:
+            return 1
+        head, level = n, 1
+        while head > 9:
+            level *= 10
+            head //= 10
+        if head == 1:
+            return  self.countDigitOne(level-1) + self.countDigitOne(n-level) + n-level +1
+        return (head) * self.countDigitOne(level-1) + self.countDigitOne(n-head*level) + level
