7edb279ab4d5fb0fa8fe31a3f08870dd2c4321e9
Qasim Wani <43754306+QasimWani@users.noreply.github.com>
2021-01-09 22:07:45 +0300

    Time: 44 ms (85.71%), Space: 14.5 MB (52.49%) - LeetHub

---
+++ two-sum/two-sum.py
@@ -0,0 +1,11 @@
+#Time: O(n)
+#Space: O(n)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        map = {}
+        for i in range(len(nums)):
+            y = target - nums[i]
+            if(y in map):
+                return map[y], i
+            else:
+                map[nums[i]] = i
