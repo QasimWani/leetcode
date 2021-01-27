57aa80790cc6aca05eb7ab7c5f2100eafb0cc65d
Qasim Wani <43754306+QasimWani@users.noreply.github.com>
2021-01-27 02:10:27 -0500

    Time: 924 ms (12.68%), Space: 26 MB (35.61%) - LeetHub

--- coin-change-2/coin-change-2.py
+++ coin-change-2/coin-change-2.py
@@ -1,3 +1,6 @@
+# Time = O(n * m); n = amount, m = number of coins
+# Space = O(n * m); n = amount, m = number of coins
+​
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         #this seems to be a variation of Knapsack problem. Instead of having finite number of each coin,
@@ -6,7 +9,7 @@ class Solution:
         #for target amount, we built up to that target starting from amount = 0. This DP based sub-problem
         #approach helps solve for target = amount effectively.
         #Using Knapsack approach, the value of each item is 1. the weight is defined by the coins list.
-        #however, the optimization algorithm is as follows: v[i, j] = max(v[i-1,j], v[i-1,j] + v[i,j-w[j]])
+        #however, the optimization algorithm is as follows: v[i, j] = max(v[i-1,j], v[i-1,j] + v[i,j-coins[i - 1]])
         
         grid = [ [0 for _ in range(amount + 1)] for _ in range(len(coins) + 1)]
         
@@ -14,9 +17,9 @@ class Solution:
         for i, _ in enumerate(grid):
             grid[i][0] = 1
         
-        #run DP -> v[i, j] = max(v[i-1,j], v[i-1,j] + v[i,j-w[j]])
+        #run DP -> v[i, j] = max(v[i-1,j], v[i-1,j] + v[i,j-coins[i - 1]])
         for i in range(1, len(grid)):
             for j in range(1, len(grid[0])):
-                grid[i][j] = max(grid[i - 1][j], grid[i - 1][j] + grid[i][j - coins[i - 1]] if j - coins[i - 1] >= 0 else 0)
+                grid[i][j] = max(grid[i - 1][j], grid[i - 1][j] + grid[i][j - coins[i-1]] if j - coins[i-1] >= 0 else 0)
                 
         return grid[-1][-1]
