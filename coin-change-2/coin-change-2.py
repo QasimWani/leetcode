aab14805cc28540802a5c860a20b265ff44b7031
Qasim Wani <43754306+QasimWani@users.noreply.github.com>
2021-01-27 02:08:06 -0500

    Time: 924 ms (12.68%), Space: 26 MB (35.81%) - LeetHub

---
+++ coin-change-2/coin-change-2.py
@@ -0,0 +1,22 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        #this seems to be a variation of Knapsack problem. Instead of having finite number of each coin,
+        #we now have an infinite supply. Therefore, we need to modify the knapsack formulation a bit.
+        #The way I worked around it is by instead of directly solving for the maximum number of ways
+        #for target amount, we built up to that target starting from amount = 0. This DP based sub-problem
+        #approach helps solve for target = amount effectively.
+        #Using Knapsack approach, the value of each item is 1. the weight is defined by the coins list.
+        #however, the optimization algorithm is as follows: v[i, j] = max(v[i-1,j], v[i-1,j] + v[i,j-w[j]])
+        
+        grid = [ [0 for _ in range(amount + 1)] for _ in range(len(coins) + 1)]
+        
+        #augment first column of each row
+        for i, _ in enumerate(grid):
+            grid[i][0] = 1
+        
+        #run DP -> v[i, j] = max(v[i-1,j], v[i-1,j] + v[i,j-w[j]])
+        for i in range(1, len(grid)):
+            for j in range(1, len(grid[0])):
+                grid[i][j] = max(grid[i - 1][j], grid[i - 1][j] + grid[i][j - coins[i - 1]] if j - coins[i - 1] >= 0 else 0)
+                
+        return grid[-1][-1]
