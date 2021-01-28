87a83971527fc813a3ce82de53434e3629b1b539
Qasim Wani <43754306+QasimWani@users.noreply.github.com>
2021-01-28 17:19:00 -0500

    Time: 96 ms (26.88%), Space: 15.5 MB (65.64%) - LeetHub

---
+++ merge-two-binary-trees/merge-two-binary-trees.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def mergeTrees(self, t1: TreeNode, t2: TreeNode) -> TreeNode:
+        #the idea is quite simple, perform a recursive BFS on one of the trees and add the values on each call.
+        #Time: O(n + m); n = number of nodes in t1, m = number of nodes in t2
+        #Space: O(n + m). Why? Because we're performing recursive calls onto the stack.
+        
+        if(t1 is None):
+            return t2
+        if(t2 is None):
+            return t1
+        
+        t1.val += t2.val
+
+        t1.left = self.mergeTrees(t1.left, t2.left)
+        t1.right = self.mergeTrees(t1.right, t2.right)
+        
+        return t1
+
+        
+        
+        
+            
+            
+        
+        
+            
+        
