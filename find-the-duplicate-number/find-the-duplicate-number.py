a57e93bcb0e4d1a7274ae019abf0be7e85474a04
Qasim Wani <43754306+QasimWani@users.noreply.github.com>
2021-01-31 14:08:09 -0500

    Time: 116 ms (11.61%), Space: 16.6 MB (47.29%) - LeetHub

--- find-the-duplicate-number/find-the-duplicate-number.py
+++ find-the-duplicate-number/find-the-duplicate-number.py
@@ -1,10 +1,20 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        #sort numbers and find the instance where nums[i] == nums[i + 1], and return that number
+        #Time: O(n)
         #Space: O(1)
-        #Time: O(nlogn)
-        nums.sort()
-        for i in range(1, len(nums)):
-            if(nums[i - 1] == nums[i]):
-                return nums[i]
-        return -1
+        
+        #solve using pigeonhole principle using Floyd's Tortoise and Hare cycle detection technique.
+        tortoise = hare = nums[0]
+        
+        while True:
+            hare = nums[nums[hare]]
+            tortoise = nums[tortoise]
+            if(hare == tortoise):
+                break
+                
+        tortoise = nums[0]
+        while tortoise != hare:
+            tortoise = nums[tortoise]
+            hare = nums[hare]
+        
+        return hare
