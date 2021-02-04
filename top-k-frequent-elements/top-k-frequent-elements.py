697fe59adff658cfa2a9896fba6b7abb34335cdb
Qasim Wani <43754306+QasimWani@users.noreply.github.com>
2021-02-04 18:02:24 -0500

    Time: 116 ms (24.23%), Space: 18.9 MB (13.86%) - LeetHub

--- top-k-frequent-elements/top-k-frequent-elements.py
+++ top-k-frequent-elements/top-k-frequent-elements.py
@@ -2,7 +2,8 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         #we can build a frequency counter for all elements, and pass it through a heap. 
         #when we pop each element from a heap, it will return the value that is of greatest frequency. 
-        
+        #Time: O(n log k)
+        #Space:O(n + k)
         set = {}
         for num in nums:
             if(num in set):
