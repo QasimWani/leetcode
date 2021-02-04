36828e891966bf6be0971d0e6dc80a7ce18aa830
Qasim Wani <43754306+QasimWani@users.noreply.github.com>
2021-02-03 23:54:05 -0500

    Time: 40 ms (46.82%), Space: 14.3 MB (29.03%) - LeetHub

--- count-sorted-vowel-strings/count-sorted-vowel-strings.py
+++ count-sorted-vowel-strings/count-sorted-vowel-strings.py
@@ -1,12 +1,10 @@
 class Solution:
     def countVowelStrings(self, n: int) -> int:
-        # s[i - 1] <= s[i]
-        # simple DP problem.
-        # no need to additional auxillary memory because it seems to be deterministic
         
-        dp = [1]*5
+        dp = [1] * 5
+        
         for i in range(1, n):
             for j in range(len(dp)):
-                dp[j] = sum(dp[j:])
-        return sum(dp)
-            
+                dp[j] = sum(dp[j:]) #takes care of the lexicographical order
+        
+        return sum(dp) #return the sum of indiviaul instances of all characters with length n.
