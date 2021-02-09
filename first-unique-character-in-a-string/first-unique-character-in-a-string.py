918c5d91acc685604a706aba35c082425cdc80d9
Qasim Wani <43754306+QasimWani@users.noreply.github.com>
2021-02-09 04:47:44 -0500

    Update first-unique-character-in-a-string.py

--- first-unique-character-in-a-string/first-unique-character-in-a-string.py
+++ first-unique-character-in-a-string/first-unique-character-in-a-string.py
@@ -1,6 +1,6 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        #Two pass algorithm. Space = Time = O(n)
+        #Two pass algorithm. Space = O(1); Time = O(n)
         table = {}
         for i, char in enumerate(s):
             if char in table:
