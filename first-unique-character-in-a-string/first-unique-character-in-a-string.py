68889ed46a1b03c8c22d36e0a5aea985430c5d36
Qasim Wani <43754306+QasimWani@users.noreply.github.com>
2021-02-09 04:45:58 -0500

    Time: 128 ms (55.41%), Space: 14.3 MB (71.18%) - LeetHub

---
+++ first-unique-character-in-a-string/first-unique-character-in-a-string.py
@@ -0,0 +1,14 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        #Two pass algorithm. Space = Time = O(n)
+        table = {}
+        for i, char in enumerate(s):
+            if char in table:
+                table[char][0] += 1
+            else:
+                table[char] = [1, i]
+        
+        for x in table:
+            if table[x][0] == 1:
+                return table[x][1]
+        return -1
