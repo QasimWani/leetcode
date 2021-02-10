773363a3c21c67ab201eaa689d86ccb7072eb895
Qasim Wani <43754306+QasimWani@users.noreply.github.com>
2021-02-10 02:59:46 -0500

    Time: 68 ms (79.45%), Space: 14.8 MB (40.29%) - LeetHub

--- remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.py
+++ remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.py
@@ -1,15 +1,11 @@
 class Solution:
     def removeDuplicates(self, S: str, i=0) -> str:
-        # recursively remove adjacent characters until no adjacent & equal characters are to be found.
-
-        i = 0
-        l = len(S)
-        flag = False
-        while i < l - 1:
-            if S[i] == S[i + 1]:
-                flag = True
-                S = S[:i] + S[i + 2 :]
-                l -= 2
-            i += 1
+        #solve using a stack in O(N) time and space.
+        stack = []
         
-        return S if not flag else self.removeDuplicates(S)
+        for char in S:
+            if stack and stack[-1] == char:
+                stack.pop(-1)
+            else:
+                stack.append(char)
+        return "".join(stack)
