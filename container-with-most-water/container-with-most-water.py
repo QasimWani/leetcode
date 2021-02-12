726fd0b07d2d9fec84a1fe404b3d4a20f3f6b8f1
Qasim Wani <43754306+QasimWani@users.noreply.github.com>
2021-02-12 08:08:53 -0500

    Time: 212 ms (16.22%), Space: 16.3 MB (98.00%) - LeetHub

---
+++ container-with-most-water/container-with-most-water.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        #2 pointer approach where you move the shorter line inward.
+        #Time: O(n)
+        #Space: O(1)
+        
+        left, right = 0, len(height) - 1
+        
+        area = (right - left) * min(height[left], height[right])
+        while left < right:
+            if height[left] < height[right]: #move left inward, i.e. left += 1
+                left += 1
+            else: #move right inward, i.e right -= 1
+                right -= 1
+            #recalculate total area
+            print(left, right)
+            area = max(area, (right - left) * min(height[left], height[right]))
+        return area
