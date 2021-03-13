0d199ecce4a180e32567fa74cd54a0466f8a38c3
Qasim Wani <43754306+QasimWani@users.noreply.github.com>
2021-03-13 18:53:46 -0500

    Time: 128 ms (76.20%), Space: 15.2 MB (15.87%) - LeetHub

--- set-matrix-zeroes/set-matrix-zeroes.py
+++ set-matrix-zeroes/set-matrix-zeroes.py
@@ -3,29 +3,39 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        # two-pass, constant space solution using special key. The idea is as follows: whenever you find a '0', replace that row and column with 'x'
-        # for all elements not equal to '0'. At the end of this operation you'll have replaced all rows and columns where 0 was present with an x.
-        # the matrix now has elements, 0, and x's. In the second pass, replace all x's with 0. This will be O(m * n) time but constant space.
+        #so the idea is as follows, whenever you see a zero, set the first element in the row and column
+        #with a zero. in the second pass, set all the elements in row, col with a zero to zero.
+        #note, we have to handle the first row differently because we can lead to setting the entire
+        #matrix to 0. 
         
-        #Time : O(M x N x Z), where M = number of rows, N = number of columns, and Z = number of zeroes.
-        #Space: O(1)
+        # Time: O(M x N)
+        # Space: O(1)
         
-        m, n = len(matrix), len(matrix[0]) #row, col
+        m, n = len(matrix), len(matrix[0])
+        set_first_column = False
+                
         for i in range(m):
-            for j in range(n):
-                if matrix[i][j] == 0: #replace row and column with x's
-                    #row operation
-                    for col, elem in enumerate(matrix[i]):
-                        if elem != 0:
-                            matrix[i][col] = 'x'
-                    
-                    #column operation
-                    for row in range(m):
-                        if matrix[row][j] != 0:
-                            matrix[row][j] = 'x'
-        ### at this point, all rows and columns with 0's should've been replaced with x's, except for original 0's.
-        #now, replace all x with 0
-        for i in range(m):
-            for j in range(n):
-                if matrix[i][j] == 'x':
+            #check to see if we need to set the first column to 0 later
+            if matrix[i][0] == 0:
+                    set_first_column = True
+            for j in range(1, n):
+                if matrix[i][j] == 0:
+                    #set first element in the row and column with 0.
+                    matrix[i][0] = 0 #set column to 0
+                    matrix[0][j] = 0 #set row to 0
+        
+        for i in range(1, m):
+            for j in range(1, n):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
                     matrix[i][j] = 0
+
+        #taking care of first row
+        if matrix[0][0] == 0:
+            matrix[0] = [0]*n
+
+        
+        #taking care of first column
+        if set_first_column:
+            for i in range(m):
+                matrix[i][0] = 0
+                
