8257b5f2c4e7a3feb8b44a864a44fc036cf2a7f7
Qasim Wani <43754306+QasimWani@users.noreply.github.com>
2021-03-14 17:21:24 -0400

    Time: 24 ms (95.43%), Space: 14.1 MB (76.47%) - LeetHub

--- middle-of-the-linked-list/middle-of-the-linked-list.py
+++ middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -8,9 +8,12 @@ class Solution:
         #super simple. have 2 pointers, one slow and one fast. the fast pointer moves 2x the speed of slow pointer which moves one step at a time.
         #when the fast pointer reaches the end of the node, the slow pointer will be at the middle node.
         
+        #Time: O(n)
+        #Space: O(1)
+        
         slow = fast = head
         while fast is not None and fast.next is not None:
-            fast = fast.next.next if fast.next is not None else None
+            fast = fast.next.next
             slow = slow.next
         return slow
         
