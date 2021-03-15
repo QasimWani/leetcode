03db937ac9a9c5f514cc76aac171173b8334e913
Qasim Wani <43754306+QasimWani@users.noreply.github.com>
2021-03-14 20:50:44 -0400

    Time: 712 ms (85.47%), Space: 16.3 MB (50.98%) - LeetHub

--- minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.py
+++ minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -5,34 +5,21 @@ class Solution:
         #character somewhere > left such that s[left] = s[new_position]. after this, we'll increment left by 1 until it
         #has traversed half of the original array. the max characters equals len(s)//2. so the answer is always less than
         #or equal to it.
+        #note that since we can inset character at any index, at left, right we have 2 choices. insert at left, or insert at right.
+        #the cost for doing so for left or right is the same, so in order to choose the one with least total cost, we make use of
+        #memoization. This is where the DP solution comes into play.
         
-        #Time: O(n), right pointer moves at most n^2 times while left pointer moves at most n/2 times.
-        #Space: O(1)
+        #Time: O(n^2), right pointer moves at most n^2 times while left pointer moves at most n/2 times.
+        #Space: O(n)
 
-#         s = list(s) #typecast to list for insertion
+        n = len(s)
+        dp = [ [0]*(n + 1) for _ in range(n + 1) ]
         
-#         left, right = 0, len(s) - 1
-#         n = len(s) #max swaps
-
-#         count = 0
+            
+        for left in range(n, -1, -1):
+            for right in range(n):
+                if left < right:
+                    dp[left][right] = dp[left + 1][right - 1] if s[left] == s[right] else 1 + min( dp[left + 1][right], dp[left][right - 1] )
         
-#         while left < right:
-#             if s[left] != s[right]:
-#                 # s.insert(right + 1, s[left])
-#                 count += 1
-#             else:
-#                 right -= 1
-#             left += 1
-#             # print(s)
-#         # print(s == s[::-1])
-#         return count
-    
-            n=len(s)
-            dp=[[0]*(n+1) for _ in range(n+1)]
-            for i in range(n, -1, -1):
-                print(i)
-                for j in range(n):
-                    if i<j:
-                        dp[i][j]=dp[i+1][j-1] if s[i]==s[j] else 1+min(dp[i+1][j],dp[i][j-1])
-            return dp[0][n-1]
+        return dp[0][n-1]
         
