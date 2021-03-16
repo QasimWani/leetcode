963f402d65eb3f6e7df143e648e947416934f056
Qasim Wani <43754306+QasimWani@users.noreply.github.com>
2021-03-16 03:07:23 -0400

    Time: 68 ms (75.06%), Space: 14.1 MB (98.21%) - LeetHub

--- add-two-numbers/add-two-numbers.py
+++ add-two-numbers/add-two-numbers.py
@@ -5,12 +5,14 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        #Time: O(max(n,m)) where n and m are the lengths of l1 and l2, respectively.
+        #Space: O(max(n, m))
+        
         list = ListNode()
         head = list
         
         while l1 or l2:
             a = b = 0
-            
             if l1:
                 a = l1.val
                 l1 = l1.next
@@ -19,20 +21,11 @@ class Solution:
                 l2 = l2.next
             
             sum = list.val + a + b
-            
             carry = sum // 10
             
             list.val = sum % 10
-            
 
             list.next = ListNode(carry) if carry or l1 or l2 else None
-            
             list = list.next
             
         return head
-            
-            
-            
-        
-    
-    
