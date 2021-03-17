fdabbeaef12438621a24608b25f068c2203d068a
Qasim Wani <43754306+QasimWani@users.noreply.github.com>
2021-03-16 21:04:18 -0400

    Time: 60 ms (74.71%), Space: 17.8 MB (86.57%) - LeetHub

--- min-stack/min-stack.py
+++ min-stack/min-stack.py
@@ -4,16 +4,29 @@ class MinStack:
         """
         initialize your data structure here.
         """
+        #Optimized stack structure. 
+        #Time: O(1)
+        #Space: O(n)
+        
         self.stack = []
         self.min = []
         
     def push(self, x: int) -> None:
         self.stack.append(x)
-        self.min.append( min(self.min[-1] if self.min else float('inf'), x) )
+        
+        current_min = self.min[-1] if self.min else float('inf')
+        if current_min >= x:
+            self.min.append(x)
+        
 
     def pop(self) -> None:
-        self.stack.pop(-1) #LIFO
-        self.min.pop(-1) #LIFO
+        popped_value = self.stack.pop(-1) #LIFO
+        
+        current_min = self.min[-1]
+        
+        if current_min == popped_value:#pop value
+            self.min.pop(-1)
+
 
     def top(self) -> int:
         return self.stack[-1]
