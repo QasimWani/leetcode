1326da0ba1eea4f3d30731e4d3d34aa24467f625
Qasim Wani <43754306+QasimWani@users.noreply.github.com>
2021-03-18 03:17:37 -0400

    Time: 3000 ms (5.01%), Space: 416.7 MB (5.07%) - LeetHub

---
+++ lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        #Basically finding the lowest common ancestor of BST but now with a binary tree, we just use generalized DFS to find the path for each node, p and q.
+        path_p = self.generate_path(root, p, [root])
+        path_q = self.generate_path(root, q, [root])
+        
+        youngest = None
+        for p, q in zip(path_p, path_q):
+            if p.val == q.val:
+                youngest = p
+        return youngest
+        
+    def generate_path(self, root, node, path):
+        """Using DFS to find the node"""
+        if root is None:
+            return
+        if root.val == node.val:
+            return path
+        
+        return self.generate_path(root.left, node, path + [root.left]) or self.generate_path(root.right, node, path + [root.right])
