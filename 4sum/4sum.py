7444ab5a7f59b6227d9bbe2c8a8a35d670623c94
Qasim Wani <43754306+QasimWani@users.noreply.github.com>
2021-03-19 18:46:24 -0400

    Time: 172 ms (68.25%), Space: 18.4 MB (8.29%) - LeetHub

--- 4sum/4sum.py
+++ 4sum/4sum.py
@@ -1,24 +1,27 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        # so solve this like 2 sum but instead of simply storing just the numbers
-        # in a hashset, we'll store the sum of two indices in the set such that
-        # the left + right = k.
+        #Time: O(n^2)
+        #Space: O(n^2)
         
-        output = set()
-        table = collections.defaultdict(list)
+        n = len(nums)
+        nums.sort()
+        table = {}
         
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                left = target - (nums[i] + nums[j])
-                
-                if left in table:
-                    for key in table[left]:
-                        x, y = key
-                        if len(set([i, j, x, y])) == 4:
-                            values = [nums[i], nums[j], nums[x], nums[y]]
-                            values.sort()
-                            output.add(tuple(values))
-                                
-                table[nums[i] + nums[j]].append([i, j])
-                
-        return output
+        for i in range(n):
+            for j in range(i + 1, n):
+                a, b = nums[i], nums[j]
+                table[a + b] = [[i, j]] if a + b not in table else table[a + b] + [[i , j]]
+        
+        result = set()
+        # a + b = target - (c + d)
+        for i in range(n):
+            for j in range(i + 1, n):
+                c, d = nums[i], nums[j]
+                rhs = target - (c + d)
+                if rhs in table:
+                    for pair in table[rhs]:
+                        k, l = pair
+                        if len({i, j, k, l}) == 4:#unique indices
+                            val = sorted([c, d, nums[k], nums[l]])
+                            result.add(tuple(val))
+        return list(result)        
