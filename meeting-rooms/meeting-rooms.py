3c5b0b2768a02a19b8459805df70729c30b94a6b
Qasim Wani <43754306+QasimWani@users.noreply.github.com>
2021-03-24 10:11:56 -0400

    Time: 72 ms (83.71%), Space: 17.4 MB (56.62%) - LeetHub

--- meeting-rooms/meeting-rooms.py
+++ meeting-rooms/meeting-rooms.py
@@ -2,18 +2,16 @@ class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
         #basically merge intervals. if we can merge any intervals, then return false. else true.
         #in other words, if there exists another start between a start and an end, return false.
-        #Time: O(n log n)
-        #Space: O(n)
+        #Time: O(n log n), where n is the number of intervals
+        #Space: O(1)
         if not intervals:
             return True
+
+        intervals.sort()
+        start, end = intervals[0]
         
-        starts, ends = zip(*intervals)
-        indices = [i[0] for i in sorted(enumerate(starts), key=lambda x:x[1])] #sort the intervals by start indices
-        
-        start, end = intervals[indices[0]]
-        
-        for i in range(1, len(starts)):
-            if starts[indices[i]] < end:#overlap found
+        for i in range(1, len(intervals)):
+            if intervals[i][0] < end:#overlap found
                 return False
-            start, end = starts[indices[i]], ends[indices[i]]
+            start, end = intervals[i]
         return True
