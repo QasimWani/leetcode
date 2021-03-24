d5427d4115ce2bc652eb1fdde40a615e3910f1f1
Qasim Wani <43754306+QasimWani@users.noreply.github.com>
2021-03-24 09:33:31 -0400

    Time: 32 ms (65.63%), Space: 14.2 MB (53.49%) - LeetHub

---
+++ robot-bounded-in-circle/robot-bounded-in-circle.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isRobotBounded(self, instructions: str) -> bool:
+        #basically check to see if eventually the robot will come back to the original position.
+        #a cycle will exist at most 4 instruction cycles. just keeping track of coordinates will
+        #do the trick.
+        
+        #default
+        move_x = True
+        move_y = False
+
+        x, y = 0, 0
+        constant = 1
+        
+        for _ in range(4):
+            for command in instructions:
+                if command == 'G':
+                    x = x + constant if move_x else x
+                    y = y + constant if move_y else y
+                else:
+                    temp_x = not move_x #get new positions
+                    temp_y = not move_y
+
+                    if (move_x and temp_y and command == 'R') or (move_y and temp_x and command == 'L'):
+                        constant *= -1
+                    move_x = temp_x
+                    move_y = temp_y
+                
+            if x == 0 and y == 0:
+                return True
+        
+        return False
