ed92133ecb87efeae4e547ac7973eacb6e81727a
Qasim Wani <43754306+QasimWani@users.noreply.github.com>
2021-03-25 04:26:32 -0400

    Time: 4708 ms (11.21%), Space: 15.7 MB (55.31%) - LeetHub

--- word-search/word-search.py
+++ word-search/word-search.py
@@ -1,6 +1,12 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         #the idea is to perform DFS on the first character instance of word in board and return true if all characters were found in board.
+        #Time: O(m x n x 3^k) where m and n are the dimensions of the board and k is the length of the word. The reason its 3 is because after
+        #the first call, we can traverse to 3 different spots on the board. And at worst, keep traversing to 3 different nodes in the tree.
+        #This is because we don't want to go back to the same place as we started since the positions are required to be unique.
+        
+        #Space: O(k) where k is the length of the word since that's the total number of recursive calls we'll be doing.
+        
         m, n = len(board), len(board[0])
         if m * n < len(word):
             return False
