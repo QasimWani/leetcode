d83969d5952a55e59995ce181c73e804fc30e1cc
Qasim Wani <43754306+QasimWani@users.noreply.github.com>
2021-03-28 13:17:04 -0400

    Time: 76 ms (98.24%), Space: 16.1 MB (53.10%) - LeetHub

--- jump-game/jump-game.py
+++ jump-game/jump-game.py
@@ -1,12 +1,13 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
+        #Greedy solution. Don't even need to maintain a DP table.
         #Time: O(n)
-        #Space: O(n)
+        #Space: O(1)
         
-        dp = [0]*len(nums)
-        dp[0] = nums[0]
+        last_pos = nums[0]
+        
         for i in range(1, len(nums)):
-            dp[i] = max(dp[i - 1], nums[i - 1]) - 1
-            if dp[i] < 0:#no possible way to continue
+            last_pos = max(last_pos, nums[i - 1]) - 1
+            if last_pos < 0:#no possible way to continue
                 return False
-        return dp[-1] >= 0
+        return last_pos >= 0
