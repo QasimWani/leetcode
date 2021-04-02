fd72ccf62dbe1cc981d662ef2027703b2c34551f
Qasim Wani <43754306+QasimWani@users.noreply.github.com>
2021-04-02 12:52:13 -0400

    Time: 3900 ms (18.84%), Space: 14 MB (91.06%) - LeetHub

---
+++ factorial-trailing-zeroes/factorial-trailing-zeroes.py
@@ -0,0 +1,26 @@
+class Solution:
+    def trailingZeroes(self, n: int) -> int:
+        #O(n) solution is to compute the factorial and find the number of
+        #trailing zeros.
+        
+        #Time: O(n)
+        #Space: O(n) aux mem
+        
+        if n < 1:
+            return 0
+        #start from 1 factorial
+        i = 1
+        factorial = 1
+        while i <= n:
+            factorial *= i
+            i += 1
+        
+        factorial = str(factorial)
+        num_zeros = 0
+        for i in range(len(factorial) - 1, -1, -1):
+            if factorial[i] == '0':
+                num_zeros += 1
+            else:
+                return num_zeros
+        return num_zeros
+        
