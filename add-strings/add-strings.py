991e9d7e67f398f3e6d852228d963887cb0b71bc
Qasim Wani <43754306+QasimWani@users.noreply.github.com>
2021-04-07 23:49:07 -0400

    Time: 44 ms (51.50%), Space: 14.2 MB (83.33%) - LeetHub

---
+++ add-strings/add-strings.py
@@ -0,0 +1,26 @@
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        #add up individual characters and take in the carry.
+        if len(num1) < len(num2):
+            num1, num2 = num2, num1
+        
+        result = ''
+        carry = 0
+        
+        for i in range(1, len(num1) + 1):
+            i *= -1
+            
+            a = int(num1[i])
+            b = 0
+            if abs(i) <= len(num2):
+                b = int(num2[i])
+                
+            sum = a + b + carry
+            carry = sum // 10
+            result += str(sum % 10)
+
+        if carry:
+            result += str(carry)
+        return result[::-1]
+            
+        
