9723f5dfa9b97687161e8d21abf0bb41375b753f
Qasim Wani <43754306+QasimWani@users.noreply.github.com>
2021-04-10 21:13:03 -0400

    Time: 116 ms (52.06%), Space: 14.8 MB (64.73%) - LeetHub f2e7d12

--- minimum-window-substring/minimum-window-substring.py
+++ minimum-window-substring/minimum-window-substring.py
@@ -1,14 +1,33 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        need, missing = collections.Counter(t), len(t)
-        i = I = J = 0
-        for j, c in enumerate(s, 1):
-            missing -= need[c] > 0
-            need[c] -= 1
-            if not missing:
-                while i < j and need[s[i]] < 0:
-                    need[s[i]] += 1
-                    i += 1
-                if not J or j - i <= J - I:
-                    I, J = i, j
-        return s[I:J]
+        #sliding window solution. start small, and see if the current substr is equal to t. if not, keep moving right pointer
+        #when substr == t, we shrink substr by moving left pointer to the right until you no longer satisfy the constraint.
+        #when failed, move right pointer and repeat the process. maintain the smallest string.
+        
+        #Time: O(|S| x |T|)
+        #Space: O(|S|) worst case
+        
+        left, right = 0, 0
+        new_trim = ''
+        t = list(t)
+        
+        while right <= len(s):
+            if self.present(s[left : right], t):
+                if not new_trim:
+                    new_trim = s[left : right]
+                elif len(s[left : right]) < len(new_trim):
+                    new_trim = s[left : right]
+                left += 1
+            else:
+                right += 1
+        return new_trim
+        
+    
+    def present(self, str, substr):
+        str = collections.Counter(str)
+        counter = 0
+        for x in substr:
+            if x in str and str[x] > 0:
+                str[x] -= 1
+                counter += 1
+        return counter == len(substr)
