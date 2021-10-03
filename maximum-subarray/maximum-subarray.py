d296bdf64c4f6551961d5d26d2fbaecd828f452a
Qasim Wani <43754306+QasimWani@users.noreply.github.com>
2021-10-03 05:42:08 -0400

    Time: 824 ms (5.05%), Space: 28 MB (20.78%) - LeetHub

--- maximum-subarray/maximum-subarray.py
+++ maximum-subarray/maximum-subarray.py
@@ -3,10 +3,9 @@ class Solution:
         # at every index we need to make a descision, is this (the new element) the max subarray or will adding the new entry make the sum even bigger? This is done while keeping track of global sum.
         # time: O(n)
         # space: O(1)
-        global_sum = float('-inf')
-        local_sum = float('-inf')
-        for i, x in enumerate(nums):
-
+        global_sum = local_sum = nums[0]
+        
+        for x in nums[1:]:
             local_sum = max(x, local_sum + x)
             global_sum = max(global_sum, local_sum)
         return global_sum
