43bc05c3548380670a7d3240ddade7058a02fec2
Qasim Wani <43754306+QasimWani@users.noreply.github.com>
2021-10-03 02:13:01 -0400

    Time: 52 ms (97.13%), Space: 15.2 MB (54.36%) - LeetHub

--- two-sum/two-sum.py
+++ two-sum/two-sum.py
@@ -2,10 +2,13 @@
 #Space: O(n)
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        map = {}
-        for i in range(len(nums)):
-            y = target - nums[i]
-            if(y in map):
-                return map[y], i
-            else:
-                map[nums[i]] = i
+        table = {}
+        # x + y = target
+        # target - num in table
+        for i, num in enumerate(nums):
+            if target - num in table:
+                return table[target - num], i
+            table[num] = i
+        return None
+                
+        
