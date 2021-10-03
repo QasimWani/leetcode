474ecf8f69f68f1ca15d3bcbe31da0d972985ea7
Qasim Wani <43754306+QasimWani@users.noreply.github.com>
2021-10-03 19:06:00 -0400

    Time: 1628 ms (11.20%), Space: 25.6 MB (11.34%) - LeetHub

--- best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,11 +1,14 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        #basically find max peak in array.
-        min_price = float('inf')
-        max_profit = 0
-        for num in prices:
-            if num < min_price:
-                min_price = num
-            elif num - min_price > max_profit:
-                max_profit = num - min_price
-        return max_profit
+        # essentially find min and max prices and calculate net profit.
+        min_price, max_price = prices[0], float('-inf')
+        current_delta = 0
+        for x in prices[1:]:
+            if min_price > x:
+                min_price = x # buy at this point.
+                max_price = float('-inf') # reset max price
+            elif max_price < x and (max_price >= min_price or max_price == float('-inf')):
+                max_price = x
+                current_delta = max(current_delta, max_price - min_price)
+            print(min_price, max_price, x, current_delta)
+        return max(0, current_delta)
