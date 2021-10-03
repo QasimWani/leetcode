b780f78e6ce0a0019e719124af0024fc1dcfc257
Qasim Wani <43754306+QasimWani@users.noreply.github.com>
2021-10-03 02:27:25 -0400

    Time: 748 ms (64.83%), Space: 27.7 MB (22.58%) - LeetHub

--- container-with-most-water/container-with-most-water.py
+++ container-with-most-water/container-with-most-water.py
@@ -1,17 +1,26 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        #2 pointer approach where you move the shorter line inward.
-        #Time: O(n)
-        #Space: O(1)
+        #area maximization. i.e. max_width x max_height.
+        #leftmost and rightmost pointer.
+        #in that case, the height is 1 and width is 8. area = 8
+        #now, say we move left pointer by 1 to the right and leave right pointer as is. Now, the area will be 7 x 7 = 49.
+        #the algorithm essentially says to move the pointer with smallest height of the two. because we start with maximum width possible, the only other way to maximize area is by maximizing height, hence 
+        #iterator solely based on height of the two pillars. 
+        # terminating condition is if they overlap, ofc.
         
         left, right = 0, len(height) - 1
+        max_area = float('-inf')
         
-        area = (right - left) * min(height[left], height[right])
-        while left < right:
-            if height[left] < height[right]: #move left inward, i.e. left += 1
+        while left <= right:
+            width = right - left
+            area = width * min(height[left], height[right])
+            max_area = max(max_area, area)
+            if height[left] < height[right]: #iterate left pointer to right by one
                 left += 1
-            else: #move right inward, i.e right -= 1
+            else:
                 right -= 1
-            #recalculate total area
-            area = max(area, (right - left) * min(height[left], height[right]))
-        return area
+        return max_area
+            
+        
+        
+            
