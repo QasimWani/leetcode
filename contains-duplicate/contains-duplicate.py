b95ef36f1ec637a583c253dfaea1fc6defb25328
Qasim Wani <43754306+QasimWani@users.noreply.github.com>
2021-10-04 01:19:28 -0400

    Time: 124 ms (53.96%), Space: 21.5 MB (18.87%) - LeetHub

--- contains-duplicate/contains-duplicate.py
+++ contains-duplicate/contains-duplicate.py
@@ -1,3 +1,12 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        return len(set(nums)) != len(nums)
+        # Time: O(N)
+        # Space: O(N)
+        # the easiest solution is to just create a map and the first time u see an element that already exists in the map, it must be the duplicate
+        
+        table = {}
+        for elem in nums:
+            if elem in table:
+                return True
+            table[elem] = True
+        return False
