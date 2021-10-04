9178fa45069039dd9b23cfb8b877d6713a4edf23
Qasim Wani <43754306+QasimWani@users.noreply.github.com>
2021-10-04 02:15:47 -0400

    Time: 245 ms (53.77%), Space: 22.4 MB (23.79%) - LeetHub

--- product-of-array-except-self/product-of-array-except-self.py
+++ product-of-array-except-self/product-of-array-except-self.py
@@ -1,26 +1,19 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        #multiply all numbers to find global product. if count(0) > 1, return 0.
-        #divide nums[index] with global product to compute final output
+        # create two arrays, prefix and suffix, of length num where prefix represents the product of all number to the left of number x and suffix represents the product to the right of x.
+        # the answer at element i will be the product of predix_i and suffix_i.
         
-        zeros = 0
-        global_product = 1
+        # Time: O(n)
+        # Space: O(1)
         
-        for num in nums:
-            if(num == 0):
-                zeros += 1
-            else:
-                global_product *= num
+        prefix = [1] * len(nums)
+        suffix = [1] * len(nums)
         
-        if(zeros >= 2):
-            return [0]*len(nums)
+        for i in range(1, len(nums)):
+            prefix[i] = nums[i - 1] * prefix[i - 1] 
         
-        
-        for i in range(len(nums)):
-            if(zeros == 1): #all numbers except for index zero = 0
-                nums[i] = 0 if nums[i] != 0 else int(global_product)
-            else: #no zeros, clean
-                nums[i] = int(global_product / nums[i])
-        
-        return nums
+        for i in range(len(nums) - 2, -1, -1):
+            suffix[i] = nums[i + 1] * suffix[i + 1] 
+
+        return [prefix[i] * suffix[i] for i in range(len(nums))]
         
