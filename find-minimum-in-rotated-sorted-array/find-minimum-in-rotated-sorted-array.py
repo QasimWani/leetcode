a039b1bbd798a6cc7369963f316f537a765a19f8
Qasim Wani <43754306+QasimWani@users.noreply.github.com>
2021-10-04 00:52:11 -0400

    Time: 44 ms (56.84%), Space: 14.5 MB (59.33%) - LeetHub

--- find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -1,32 +1,15 @@
+# Time: O(log n)
+# Space: O(1)
+
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         # start with the first element. if the first element is smaller than the last element, then it must be sorted
         if nums[0] <= nums[-1]:
             return nums[0]
         
-        middle = len(nums) // 2
-        
-        if nums[-1] < nums[middle]: # smallest element on the right - middle side
-            ans = nums[-1]
-            i = len(nums) - 1
-            while i >= middle:
-                if ans >= nums[i]:
-                    ans = nums[i]
-                else:
-                    return ans
-                i -= 1
-                
-        else: # smallest element on the middle - left side
-            ans = nums[middle]
-            i = middle
-            while i >= 0:
-                if ans >= nums[i]:
-                    ans = nums[i]
-                else:
-                    return ans
-                
-                i -= 1
-        return None
-                
-                
-            
+        # find first peak and that should be the answer
+        for i in range(len(nums) - 1):
+            curr, next = nums[i], nums[i + 1]
+            if curr > next: # currently at peak, next value must be the smallest value
+                return next
+        return curr
