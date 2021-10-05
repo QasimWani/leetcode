e31a45e2b008216bfef7780e3ab3f4ce7e1490ad
Qasim Wani <43754306+QasimWani@users.noreply.github.com>
2021-10-05 05:43:47 -0400

    Time: 88 ms (97.72%), Space: 17.6 MB (58.95%) - LeetHub

--- group-anagrams/group-anagrams.py
+++ group-anagrams/group-anagrams.py
@@ -1,15 +1,19 @@
 class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # Since anagrams deal with words with different positions but same
-        # characters, sorting each anagram would produce equivalent words.
-        # From sorting the words, we can simply build a hashtable and append
-        # words to the correct anagram based on its sorted value.
-​
-        table = {}
-        for word in strs:
-            sorted_word = "".join(sorted(word))
-            if(sorted_word in table):
-                table[sorted_word].append(word)
-            else:
-                table[sorted_word] = [word]
-        return list(table.values())
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # since an anagram is multiple representation of the same word, sorting them should produce the same word.
+        # so, a group of anagrams should be the same once sorted. so, we can easily group them together based on this key.
+
+        # Time: O(n log m), where n is the total number of anagrams and m is the longest anagram
+        # Space: O(n) where n is the total number of anagrams
+        
+        table = {} # key = sorted anagram, value = [original anagram]
+        
+        for gram in strs:
+            sg = "".join(sorted(gram))
+            if sg in table:
+                table[sg].append(gram)
+            else:
+                table[sg] = [gram]
+        
+        
+        return table.values()
