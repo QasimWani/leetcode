24a30451defeea59019edc99b57b456524a66b62
Qasim Wani <43754306+QasimWani@users.noreply.github.com>
2021-10-05 01:10:59 -0400

    Time: 77 ms (47.63%), Space: 14.3 MB (79.38%) - LeetHub

--- longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,24 +1,21 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         # since the substring will be a contigious set of characters, each time a character is repeated from a previous set of characters, the entire set will be void and will need to rebuild the unique character set.
-        # can solve this simply using a table and keep track of unique characters. if a characters occurs more than once in the linear pass, then we rebuild the table and record the length of that.
-        # we do this until the end and retrieve the maximum length of the table during the process.
+        # this can be solved by keeping track of two pointers, sort of. Each time we move to a new unique character, we move the right pointer by one. And each time we see a character we've already seen, we move the left
+        # pointer to the right until the set of characters between left and right pointers are all unique. We don't have to do a second traversal in the sliding window approach. Since the input is guaranteed to be the set
+        # of all characters, we can create a character array of size 256. And each time we enter we enter a new value we assign the unicode value of that character by the current index. And if we see that we've already
+        # traversed through that character, we reset its value with the current index.
         
-        # Time: O(n^2)
-        # Space: O(n) - worst case
+        # Time: O(n)
+        # Space: O(1)
+        
+        table = [-1] * 256
+        start = -1
         max_length = 0
-        for i in range(len(s)):
-            table = {}
-            for j in range(i, len(s)):
-                # given s[i], what's the maximum unique subarray you can build?
-                if s[j] not in table:
-                    table[s[j]] = True
-                else:
-                    break
-                max_length = max(max_length, len(table))
+        for i, char in enumerate(s):
+            asc = ord(char) # get unicode value
+            if table[asc] > start: # we've seen this value before
+                start = table[asc] # reset bound with that value
+            table[asc] = i
+            max_length = max(max_length, i - start)
         return max_length
-
-                
-            
-                
-                
