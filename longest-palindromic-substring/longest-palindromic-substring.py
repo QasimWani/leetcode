f02dd94b93f152b6b141115ae06cd511cce35fae
Qasim Wani <43754306+QasimWani@users.noreply.github.com>
2021-10-05 04:51:09 -0400

    Time: 1772 ms (36.75%), Space: 14 MB (98.77%) - LeetHub

--- longest-palindromic-substring/longest-palindromic-substring.py
+++ longest-palindromic-substring/longest-palindromic-substring.py
@@ -3,26 +3,25 @@ class Solution:
         # start small. Given a character c, see if its a palindrome by expanding outwards.
         
         # Time: O(n^2)
-        # Space: O(n)
+        # Space: O(1)
         
-        answer = ""
-        
+        start, end = 0, 0
         for center in range(len(s)):
             # if length of palindrome is odd. expand about center point
             i, j = center, center
             # expand outwards
             while i >= 0 and j < len(s) and s[i] == s[j]:
-                if j - i + 1 > len(answer):
-                    answer = s[i : j + 1]
+                if j - i + 1 > end - start:
+                    start, end = i, j + 1
                 i -= 1
                 j += 1
             
             # if length of palindrome is even. expand about pivot
             i, j = center, center + 1
             while i >= 0 and j < len(s) and s[i] == s[j]:
-                if j - i + 1 > len(answer):
-                    answer = s[i : j + 1]
+                if j - i + 1 > end - start:
+                    start, end = i, j + 1
                 i -= 1
                 j += 1
         
-        return answer
+        return s[start : end]
