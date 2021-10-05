96c238ed7402912ce36eafe6d44ab6dee9de5efe
Qasim Wani <43754306+QasimWani@users.noreply.github.com>
2021-10-05 06:10:34 -0400

    Time: 40 ms (90.74%), Space: 15.5 MB (27.95%) - LeetHub

--- valid-palindrome/valid-palindrome.py
+++ valid-palindrome/valid-palindrome.py
@@ -1,5 +1,16 @@
+import re
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = [char.lower() for char in s if char.isalnum()]
-        half = len(s)//2
-        return s[:half] == s[::-1][:half]
+        # 2 pointer approach. keep iterating inwards, left and right should point to the same character.
+        # Time: O(n)
+        # Space: O(1)
+        
+        s = re.sub(r'[^A-Za-z0-9 \t]+', '', "".join(s.split(" "))).lower()
+        left, right = 0, len(s) - 1
+
+        while left < right: # middle character or open pivot. doesn't matter which one it is :D
+            if s[left] != s[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
