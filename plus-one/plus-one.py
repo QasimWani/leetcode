985bd1bd4e154042aa086ee2bc53267e0ffed1a7
Qasim Wani <43754306+QasimWani@users.noreply.github.com>
2022-01-03 16:55:43 +0300

    Time: 58 ms, Memory: 14.3 MB -LeetHub

---
+++ plus-one/plus-one.py
@@ -0,0 +1,22 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        n = len(digits)
+
+        # move along the input array starting from the end
+        for i in range(n):
+            idx = n - 1 - i
+            # set all the nines at the end of array to zeros
+            if digits[idx] == 9:
+                digits[idx] = 0
+            # here we have the rightmost not-nine
+            else:
+                # increase this rightmost not-nine by 1
+                digits[idx] += 1
+                # and the job is done
+                return digits
+
+        # we're here because all the digits are nines
+        return [1] + digits
+        
+        
+        
