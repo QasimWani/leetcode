2b21d164f28ac80ec331ff7ad5dab21b4ec72093
Qasim Wani <43754306+QasimWani@users.noreply.github.com>
2022-01-05 20:23:44 +0300

    Time: 76 ms (90.42%), Space: 41.1 MB (26.12%) - LeetHub

---
+++ 1-two-sum/1-two-sum.js
@@ -0,0 +1,11 @@
+// Test implementation
+
+const twoSum = function(nums, target) {
+    const comp = {};
+    for(let i=0; i<nums.length; i++){
+        if(comp[nums[i] ]>=0){
+            return [ comp[nums[i] ] , i]
+        }
+        comp[target-nums[i]] = i
+    }
+};
