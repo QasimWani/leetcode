6320bd1b985db95e12c417ad5e44ac6999694fc8
Qasim Wani <43754306+QasimWani@users.noreply.github.com>
2022-01-05 20:32:21 +0300

    Time: 109 ms (42.15%), Space: 15.3 MB (54.01%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -4,13 +4,9 @@ class Solution:
     def twoSum(self, nums, target):
         table = {}
         # x + y = target
-        # target - num in table
         for i, num in enumerate(nums):
             if target - num in table:
-                return table[target - num], i
+                return table[target - num], i # target - num in table
             table[num] = i
             
         return None
-
-                
-        
