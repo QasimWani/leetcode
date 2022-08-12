e2d0ef79368f0c13bbbd4d55a29d076b17c7d960
Qasim Wani <43754306+QasimWani@users.noreply.github.com>
2022-08-12 14:49:00 -0700

    Time: 81 ms (79.47%), Space: 15.1 MB (50.50%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -2,11 +2,19 @@
 #Space: O(n)
 class Solution:
     def twoSum(self, nums, target):
-        table = {}
-        # x + y = target
-        for i, num in enumerate(nums):
-            if target - num in table:
-                return table[target - num], i # target - num in table
-            table[num] = i
-            
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        # create a dictionary to store the values
+        d = {}
+        
+        # for each value in the array
+        for i, value in enumerate(nums):
+            # if the value is in the dictionary
+            if target - value in d:
+                return d[target - value], i
+            else:
+                d[value] = i
         return None
