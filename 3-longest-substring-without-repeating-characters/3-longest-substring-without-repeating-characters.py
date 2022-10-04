4c64551a94bfe1e4e06e53963c2a26402d1de4a8
Qasim Wani <43754306+QasimWani@users.noreply.github.com>
2022-10-04 18:08:57 -0400

    Time: 112 ms (59.89%), Space: 14.1 MB (49.70%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -1,6 +1,6 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        # O(n^2) solution would be setting a start index and while each character only appears once, keep moving the end character by one.
+        # O(n) solution would be setting a start index and while each character only appears once, keep moving the end character by one.
         
         table = [-1] * 128
         max_len = 0
@@ -13,6 +13,4 @@ class Solution:
             table[ascii] = i # store current position in character map
             max_len = max(max_len, i - start)
         return max_len
-            
-        
-       
+# LeetHub test...
